63a6f46071fcc39def18822fb39df776965b4a03
alu10423712 <naogui11>
2022-03-08 17:25:59 +0100

    Game

---
+++ Test/Character/Stat/StatTest.java
@@ -0,0 +1,29 @@
+package Character.Stat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void increase() {
+    }
+
+    @Test
+    void decrease() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/Character/Stat/Stat.java
@@ -0,0 +1,32 @@
+package Character.Stat;
+
+public abstract class Stat {
+    //Atributos
+    private int value;
+
+    //Constructor
+    public Stat(int value) {
+        this.value = value;
+    }
+
+    //Metodos
+    public int getValue() {
+        return value;
+    }
+
+    public void increase() {
+        value += 1;
+    }
+
+    public void decrease() {
+        value -= 1;
+    }
+
+    //Imprimir
+    @Override
+    public String toString() {
+        return "Stat{" +
+                "value=" + value +
+                '}';
+    }
+}
